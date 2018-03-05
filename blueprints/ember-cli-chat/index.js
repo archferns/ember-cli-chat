67831ef675eefa806e3808ce9c1310cf1d27e8e0
Arlston Fernandes <archferns@gmail.com>
2018-03-06 00:52:20 +0530

    Install ember-engines as package in parent application

--- blueprints/ember-cli-chat/index.js
+++ blueprints/ember-cli-chat/index.js
@@ -1,9 +1,16 @@
 /* eslint-env node */
 module.exports = {
   description: 'ember-cli-chat',
-  normalizeEntityName() {},
+
+  // locals(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
   afterInstall(options) {
-    return this.addAddonsToProject({
+    return this.addPackagesToProject({
       packages: [
         { name: 'ember-engines' }
       ]
